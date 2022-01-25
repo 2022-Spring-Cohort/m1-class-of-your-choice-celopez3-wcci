8a5090a29eded219efa256801400836adf9d9dc1
Carlos Lopez <carlos@wecancodeit.org>
2022-01-25 13:53:46 -0500

    Added Festival class and CreateFestival method

---
+++ Festival.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace template_csharp_class_of_your_choice
+{
+    class Festival
+    {
+        public string Name { get; set; }
+        public string Year { get; set; }
+        public string Location { get; set; }
+        public List<string> Amenities { get; set; }
+
+        public bool IsCamping { get; set; }
+    }
+}
